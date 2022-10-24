d83a3125c0922efcf9858a6f6132efdd30c50db2
kamaru <xin.kamaru@gmail.com>
2022-10-24 13:01:19 +0530

    set urls for navbar options ||| 1:basic <H1> template designed for navbar options ||| 2:set view and path for all options ||| 3:mapped the url to html pages

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('about_us/',views.about_us,name='about_us'),
+    path('blog/',views.blog,name='blog'),
+    path('gallery/',views.gallery,name='gallery'),
+    path('contact_us/',views.contact_us,name='contact_us'),
+    path('products/',views.products,name='products'),
+    path('services/',views.services,name='services'),
+    path('feedback/',views.feedback,name='feedback'),
+    path('manage_menu/',views.manage_menu,name='manage_menu'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+def about_us(request):
+    return render(request,'about_us.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+def contact_us(request):
+    return render(request,'contact_us.html')
+
+def products(request):
+    return render(request,'products.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def feedback(request):
+    return render(request,'feedback.html')
+
+def manage_menu(request):
+    return render(request,'manage_menu.html')
