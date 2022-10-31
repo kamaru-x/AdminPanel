8b30a734e4a840bfca357a9be5b8942c8dad01b4
kamaru <xin.kamaru@gmail.com>
2022-10-31 13:15:35 +0530

    auto url js changed

--- static/assets/js/autourl.js
+++ static/assets/js/autourl.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
 
     $('input[name="title"]').on('keyup',function(){
         var value = $('input[name="title"]').val();
-        var final = value.replace(/ /g,"_");
-        var url = ('https://www.testsite/'+final+'.com')
-        $('input[name="url"]').val(url);
+        var final = value.replace(/ /g,"-");
+        $('input[name="url"]').val(final);
     });
 })
